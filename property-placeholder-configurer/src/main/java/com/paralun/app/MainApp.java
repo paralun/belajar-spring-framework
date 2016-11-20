2fb5d77fde93e071b8d71db02d2ab1943e7e7960
James Kusmambang <kusmambang@gmail.com>
2016-11-20 16:39:21 +0700

    property placeholder

---
+++ property-placeholder-configurer/src/main/java/com/paralun/app/MainApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        Database database = (Database) context.getBean("database");
+        System.out.println(database);
+    }
+}
