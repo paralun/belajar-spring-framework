a886f92cb054155f2f2caee65295bac98ffc2b50
James Kusmambang <kusmambang@gmail.com>
2016-12-06 23:53:48 +0700

    configuration

---
+++ spring-hibernate/src/main/java/com/paralun/app/configuration/AppConfig.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.paralun.app")
+public class AppConfig {
+    
+}
---
+++ spring-hibernate/src/main/java/com/paralun/app/configuration/HibernateConfiguration.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016 | James Kusmambang
+ * Source : https://github.com/paralun
+ */
+package com.paralun.app.configuration;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.paralun.app")
+@PropertySource("classpath:config/jdbc.properties")
+public class HibernateConfiguration {
+    
+    @Autowired
+    private Environment env;
+    
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("com.paralun.app.model");
+        factory.setHibernateProperties(hibernateProperties());
+        return factory;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource source = new DriverManagerDataSource();
+        source.setDriverClassName(env.getProperty("jdbc.driver"));
+        source.setUrl(env.getProperty("jdbc.url"));
+        source.setUsername(env.getProperty("jdbc.username"));
+        source.setPassword(env.getProperty("jdbc.password"));
+        return source;
+    }
+    
+    public Properties hibernateProperties() {
+        Properties prop = new Properties();
+        prop.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        prop.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        prop.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+        return prop;
+    }
+    
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager manager = new HibernateTransactionManager();
+        manager.setSessionFactory(sessionFactory);
+        return manager;
+    }
+    
+}
