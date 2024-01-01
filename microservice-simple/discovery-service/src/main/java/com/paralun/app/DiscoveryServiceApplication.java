5ef5888b6881ee9a397133a16495dc05c169ffb0
James Kusmambang <kusmambang@gmail.com>
2024-01-01 16:16:23 +0700

    discovery service first

--- microservice-simple/discovery-service/src/main/java/com/paralun/app/DiscoveryServiceApplication.java
+++ microservice-simple/discovery-service/src/main/java/com/paralun/app/DiscoveryServiceApplication.java
@@ -2,14 +2,14 @@ package com.paralun.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
-public class ConfigServiceApplication {
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
